6b9553dbe7391de2b37a87e2a01bb29b4c6c1c9b
Tien Do Nam <dev.tien.donam@gmail.com>
2022-12-28 17:27:07 +0100

    feat: add delete all selection

--- lib/pages/selected_files_page.dart
+++ lib/pages/selected_files_page.dart
@@ -4,6 +4,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:localsend_app/gen/strings.g.dart';
 import 'package:localsend_app/provider/selected_files_provider.dart';
 import 'package:localsend_app/util/file_size_helper.dart';
+import 'package:routerino/routerino.dart';
 
 class SelectedFilesPage extends ConsumerWidget {
   const SelectedFilesPage();
@@ -19,13 +20,26 @@ class SelectedFilesPage extends ConsumerWidget {
       body: ListView(
         padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 15),
         children: [
-          Padding(
-            padding: const EdgeInsets.only(left: 5),
-            child: Text(t.sendTab.selection.files(files: selectedFiles.length)),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(left: 5),
-            child: Text(t.sendTab.selection.size(size: selectedFiles.fold(0, (prev, curr) => prev + curr.size).asReadableFileSize)),
+          Row(
+            children: [
+              const SizedBox(width: 5),
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(t.sendTab.selection.files(files: selectedFiles.length)),
+                    Text(t.sendTab.selection.size(size: selectedFiles.fold(0, (prev, curr) => prev + curr.size).asReadableFileSize)),
+                  ],
+                ),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  ref.read(selectedFilesProvider.notifier).state = [];
+                  context.popUntilRoot();
+                },
+                child: Text(t.selectedFilesPage.deleteAll),
+              ),
+            ],
           ),
           const SizedBox(height: 10),
           ...selectedFiles.mapIndexed(
