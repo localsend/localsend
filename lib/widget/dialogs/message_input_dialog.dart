1b6c10c1f0b4ca6a68765ff26374e64c49441d0f
Tien Do Nam <dev.tien.donam@gmail.com>
2023-01-24 01:32:44 +0100

    feat: add multiline toggle to message input

--- lib/widget/dialogs/message_input_dialog.dart
+++ lib/widget/dialogs/message_input_dialog.dart
@@ -11,18 +11,41 @@ class MessageInputDialog extends StatefulWidget {
 
 class _MessageInputDialogState extends State<MessageInputDialog> {
   String _text = '';
+  bool _multiline = false;
 
   @override
   Widget build(BuildContext context) {
     return AlertDialog(
       title: Text(t.dialogs.messageInput.title),
-      content: TextField(
-        keyboardType: TextInputType.multiline,
-        maxLines: null,
-        autofocus: true,
-        onChanged: (s) {
-          setState(() => _text = s);
-        },
+      content: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          TextFormField(
+            key: ValueKey('m-$_multiline'),
+            initialValue: _text,
+            keyboardType: _multiline ? TextInputType.multiline : TextInputType.text,
+            maxLines: null,
+            autofocus: true,
+            onChanged: (s) {
+              setState(() => _text = s);
+            },
+            onFieldSubmitted: _multiline ? null : (s) => context.pop(s),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              Text(t.dialogs.messageInput.multiline),
+              Checkbox(
+                value: _multiline,
+                onChanged: (b) {
+                  setState(() {
+                    _multiline = b == true;
+                  });
+                },
+              ),
+            ],
+          ),
+        ],
       ),
       actions: [
         TextButton(
