770da1e894f1e3bd7f2060ae136958c13cc4d0f3
Tien Do Nam <dev.tien.donam@gmail.com>
2023-01-26 01:03:47 +0100

    fix: also prevent self-discovering in manual discover

--- lib/provider/network/targeted_discovery_provider.dart
+++ lib/provider/network/targeted_discovery_provider.dart
@@ -3,24 +3,29 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:localsend_app/model/device.dart';
 import 'package:localsend_app/model/dto/info_dto.dart';
 import 'package:localsend_app/provider/dio_provider.dart';
+import 'package:localsend_app/provider/fingerprint_provider.dart';
 import 'package:localsend_app/util/api_route_builder.dart';
 
 final targetedDiscoveryProvider = Provider((ref) {
   final dio = ref.watch(dioProvider(DioType.discovery));
-  return TargetedDiscoveryService(dio);
+  final fingerprint = ref.watch(fingerprintProvider);
+  return TargetedDiscoveryService(dio, fingerprint);
 });
 
 /// Try to discover a single device using the given IP and port.
 class TargetedDiscoveryService {
   final Dio _dio;
+  final String _fingerprint;
 
-  TargetedDiscoveryService(this._dio);
+  TargetedDiscoveryService(this._dio, this._fingerprint);
 
   Future<Device?> discover(String ip, int port) async {
     final url = ApiRoute.info.targetRaw(ip, port);
     Device? device;
     try {
-      final response = await _dio.get(url);
+      final response = await _dio.get(url, queryParameters: {
+        'fingerprint': _fingerprint,
+      });
       final dto = InfoDto.fromJson(response.data);
       device = dto.toDevice(ip, port);
     } on DioError catch (e) {
