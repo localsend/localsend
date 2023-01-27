daa7926837ee50920971a778508f24658de3a199
Tien Do Nam <dev.tien.donam@gmail.com>
2023-01-27 02:30:01 +0100

    chore: add linux script

---
+++ scripts/compile_linux_appbundle.sh
@@ -0,0 +1,15 @@
+fvm flutter clean
+fvm flutter pub get
+fvm flutter pub run build_runner build -d
+fvm flutter build linux
+
+rm -rf AppDir
+rm -rf appimage-build
+
+mkdir AppDir
+cp -r build/linux/x64/release/bundle/* AppDir
+appimage-builder
+sudo chmod +x LocalSend-latest-x86_64.AppImage
+
+rm -rf AppDir
+rm -rf appimage-build
