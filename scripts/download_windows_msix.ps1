36d0aa04ece4aa61d88efec4e171ce247f29d7c5
Tien Do Nam <dev.tien.donam@gmail.com>
2023-01-30 00:53:27 +0100

    chore: add msix downloader script

---
+++ scripts/download_windows_msix.ps1
@@ -0,0 +1,4 @@
+# Downloads the MSIX signed by Microsoft
+# This is useful to publish the MSIX outside of the store
+# Original script can be found in https://github.com/MattiasC85/Scripts/blob/master/OSD/Download-AppxFromStore.ps1
+.\scripts\msix\msix_downloader.ps1 -StoreURL https://www.microsoft.com/store/productId/9NCB4Z0TZ6RR
