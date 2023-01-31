1b686765d5cc9fb3215e78f5a476a466c2bacac3
Tien Do Nam <dev.tien.donam@gmail.com>
2023-01-31 19:02:23 +0100

    chore: update apk script

--- scripts/compile_android_apk.sh
+++ scripts/compile_android_apk.sh
@@ -1,14 +1,21 @@
-git reset --hard origin/main
-git pull
-git submodule update --remote
+# This apk script is written in a way to conform with "Reproducible Builds" by F-Droid
+# The Android SDK should be installed with sdkmanager (apt install sdkmanager)
+# By default, it will install everything to "/opt/android-sdk". This path is important!
+# Furthermore, the Flutter version from the submodule is used.
+
+# UNCOMMENT THESE LINES TO BUILD FROM LATEST COMMIT
+# git reset --hard origin/main
+# git pull
+
+git submodule update --init
 
 cd ..
+rm -rf /tmp/build
 cp localsend /tmp/build -r
 pushd /tmp/build
 
 alias flutter='submodules/flutter/bin/flutter'
 flutter config --no-analytics
-flutter config --android-sdk /opt/android-sdk
 flutter pub get
 flutter pub run build_runner build -d
 flutter build apk
