824c1b092c0d46fdf50f1599878378e1220440b2
Tien Do Nam <dev.tien.donam@gmail.com>
2023-02-08 21:32:28 +0100

    feat: add server error responses for debugging

--- lib/pages/debug_page.dart
+++ lib/pages/debug_page.dart
@@ -5,6 +5,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:localsend_app/provider/app_arguments_provider.dart';
+import 'package:localsend_app/provider/fingerprint_provider.dart';
 import 'package:localsend_app/util/snackbar.dart';
 
 const _headerStyle = TextStyle(fontWeight: FontWeight.bold);
@@ -15,29 +16,57 @@ class DebugPage extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final appArguments = ref.watch(appArgumentsProvider);
+    final fingerprint = ref.watch(fingerprintProvider);
     return Scaffold(
       appBar: AppBar(
         title: const Text('Debugging'),
       ),
       body: ListView(
-        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 30),
+        padding: const EdgeInsets.only(left: 20, right: 20, top: 10, bottom: 30),
         children: [
-          const Text('Debug Mode', style: _headerStyle),
-          Text(kDebugMode.toString()),
-          const SizedBox(height: 20),
-          const Text('App Arguments', style: _headerStyle),
-          _CopyableText(
+          _DebugEntry(
+            name: 'Debug Mode',
+            value: kDebugMode.toString(),
+          ),
+          _DebugEntry(
+            name: 'Fingerprint (generated randomly on app start)',
+            value: fingerprint,
+          ),
+          _DebugEntry(
             name: 'App Arguments',
             value: appArguments.isEmpty ? null : appArguments.join(' '),
           ),
-          const SizedBox(height: 20),
-          const Text('Dart SDK', style: _headerStyle),
-          _CopyableText(
+          _DebugEntry(
             name: 'Dart SDK',
             value: Platform.version,
           ),
         ],
       ),
+    );
+  }
+}
+
+class _DebugEntry extends StatelessWidget {
+  final String name;
+  final String? value;
+
+  const _DebugEntry({
+    required this.name,
+    required this.value,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        const SizedBox(height: 20),
+        Text(name, style: _headerStyle),
+        _CopyableText(
+          name: name,
+          value: value,
+        ),
+      ],
     );
   }
 }
