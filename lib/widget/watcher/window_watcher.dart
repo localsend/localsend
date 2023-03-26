0fcd9f686acb16f2e12f270fd80c7f4520d330c0
TheGB0077 <39922116+TheGB0077@users.noreply.github.com>
2023-03-26 01:40:14 -0300

    Save dimensions on resize and move on linux

--- lib/widget/watcher/window_watcher.dart
+++ lib/widget/watcher/window_watcher.dart
@@ -1,6 +1,6 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:localsend_app/provider/persistence_provider.dart';
 import 'package:localsend_app/provider/window_dimensions_provider.dart';
 import 'package:localsend_app/util/platform_check.dart';
 import 'package:window_manager/window_manager.dart';
@@ -20,13 +20,14 @@ class WindowWatcher extends ConsumerStatefulWidget {
 }
 
 class _WindowWatcherState extends ConsumerState<WindowWatcher> with WindowListener {
-  static WindowDimensionProvider? _windowDimensionProvider;
+  static WindowDimensionsController? _dimensionsController;
+  static bool timeout = false;
 
-  WindowDimensionProvider _ensureDimensionsProvider() => WindowDimensionProvider(ref.watch(persistenceProvider));
+  WindowDimensionsController _ensureDimensionsProvider() => ref.watch(windowDimensionProvider);
 
   @override
   Widget build(BuildContext context) {
-    _windowDimensionProvider ??= _ensureDimensionsProvider();
+    _dimensionsController ??= _ensureDimensionsProvider();
     return widget.child;
   }
 
@@ -52,23 +53,39 @@ class _WindowWatcherState extends ConsumerState<WindowWatcher> with WindowListen
     super.dispose();
   }
 
+  //Linux alternative for onWindowMoved and onWindowResized
+  @override
+  Future<void> onWindowMove() async {
+    if(checkPlatform([TargetPlatform.linux]) && !timeout) {
+      timeout = true;
+      final windowOffset = await windowManager.getPosition();
+      final windowSize = await windowManager.getSize();
+      await _dimensionsController?.storeDimensions(windowOffset: windowOffset, windowSize: windowSize);
+      Timer(const Duration(milliseconds: 600), () => toggleTimeout());
+    }
+  }
+  //handy limiter for onWindowMove
+  void toggleTimeout() {
+    timeout = !timeout;
+  }
+
   @override
   Future<void> onWindowMoved() async {
     final windowOffset = await windowManager.getPosition();
-    await _windowDimensionProvider?.storePosition(windowOffset: windowOffset);
+    await _dimensionsController?.storePosition(windowOffset: windowOffset);
   }
 
   @override
   Future<void> onWindowResized() async {
     final windowSize = await windowManager.getSize();
-    await _windowDimensionProvider?.storeSize(windowSize: windowSize);
+    await _dimensionsController?.storeSize(windowSize: windowSize);
   }
 
   @override
   Future<void> onWindowClose() async {
     final windowOffset = await windowManager.getPosition();
     final windowSize = await windowManager.getSize();
-    await _windowDimensionProvider?.storeDimensions(windowOffset: windowOffset, windowSize: windowSize);
+    await _dimensionsController?.storeDimensions(windowOffset: windowOffset, windowSize: windowSize);
     widget.onClose();
   }
 
