3634755e6aa14229ad671b3d84b990dbc6e57e06
Suman Sahoo <ssamsomu@gmail.com>
2023-04-10 17:05:13 +0530

    changed the button UI to match the design

--- lib/widget/big_button.dart
+++ lib/widget/big_button.dart
@@ -15,26 +15,25 @@ class BigButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ConstrainedBox(
-      constraints: const BoxConstraints(
-        maxWidth: 90,
-        minWidth: 90,
-        minHeight: 65,
-      ),
+    return SizedBox(
+      height: 60,
+      width: 150,
       child: ElevatedButton(
         style: ElevatedButton.styleFrom(
-          backgroundColor: filled ? Theme.of(context).colorScheme.primary : null,
-          foregroundColor: filled ? Theme.of(context).colorScheme.onPrimary : null,
+          backgroundColor:
+              filled ? Theme.of(context).colorScheme.primary : null,
+          foregroundColor:
+              filled ? Theme.of(context).colorScheme.onPrimary : null,
           shape: RoundedRectangleBorder(
             borderRadius: BorderRadius.circular(10),
           ),
         ),
         onPressed: onTap,
-        child: Column(
+        child: Row(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Icon(icon),
-            const SizedBox(height: 5),
+            const SizedBox(width: 5),
             FittedBox(
               child: Text(label, maxLines: 1),
             ),
