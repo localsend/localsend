aa71dbb9e3f5f7426f49a8925b9d76831f399503
Tien Do Nam <dev.tien.donam@gmail.com>
2023-04-22 02:03:24 +0200

    feat: calculate cert hash from DER

--- lib/util/security_helper.dart
+++ lib/util/security_helper.dart
@@ -1,11 +1,12 @@
+import 'dart:convert';
 import 'dart:typed_data';
 
 import 'package:basic_utils/basic_utils.dart';
 import 'package:localsend_app/model/persistence/stored_security_context.dart';
 
 /// Generates a random [SecurityContextResult].
-StoredSecurityContext generateSecurityContext() {
-  final keyPair = CryptoUtils.generateRSAKeyPair();
+StoredSecurityContext generateSecurityContext([AsymmetricKeyPair? keyPair]) {
+  keyPair ??= CryptoUtils.generateRSAKeyPair();
   final privateKey = keyPair.privateKey as RSAPrivateKey;
   final publicKey = keyPair.publicKey as RSAPublicKey;
   final dn = {
@@ -18,10 +19,29 @@ StoredSecurityContext generateSecurityContext() {
   };
   final csr = X509Utils.generateRsaCsrPem(dn, privateKey, publicKey);
   final certificate = X509Utils.generateSelfSignedCertificate(keyPair.privateKey, csr, 365 * 10);
+  final hash = calculateHashOfCertificate(certificate);
+
   return StoredSecurityContext(
     privateKey: CryptoUtils.encodeRSAPrivateKeyToPemPkcs1(privateKey),
     publicKey: CryptoUtils.encodeRSAPublicKeyToPemPkcs1(publicKey),
     certificate: certificate,
-    certificateHash: CryptoUtils.getHash(Uint8List.fromList(certificate.replaceAll('\r\n', '\n').codeUnits), algorithmName: 'SHA-256'),
+    certificateHash: hash,
   );
 }
+
+/// Calculates the hash of a certificate.
+String calculateHashOfCertificate(String certificate) {
+  // Convert PEM to DER
+  final pemContent = certificate
+      .replaceAll('\r\n', '\n').split('\n')
+      .where((line) => line.isNotEmpty && !line.startsWith('---'))
+      .join();
+  final der = base64Decode(pemContent);
+
+  // Calculate hash
+  return CryptoUtils.getHash(
+    Uint8List.fromList(der),
+    algorithmName: 'SHA-256',
+  );
+}
+
