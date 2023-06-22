cb7b51e663201934d3b7fc140c9040745ab961b6
Tien Do Nam <38380847+Tienisto@users.noreply.github.com>
2023-06-22 19:24:04 +0200

    feat: bind ESC globally (#543)

--- lib/pages/changelog_page.dart
+++ lib/pages/changelog_page.dart
@@ -3,7 +3,6 @@ import 'package:flutter/services.dart' show rootBundle;
 import 'package:flutter_markdown/flutter_markdown.dart';
 import 'package:localsend_app/gen/assets.gen.dart';
 import 'package:localsend_app/gen/strings.g.dart';
-import 'package:localsend_app/util/shortcut_warpper.dart';
 import 'package:localsend_app/util/ui/nav_bar_padding.dart';
 
 class ChangelogPage extends StatelessWidget {
@@ -11,29 +10,26 @@ class ChangelogPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ShortcutWrapper(
-      child: Scaffold(
-        appBar: AppBar(
-          title: Text(t.changelogPage.title),
-        ),
-        body: FutureBuilder(
-          future: rootBundle
-              .loadString(Assets.changelog), // ignore: discarded_futures
-          builder: (context, data) {
-            if (!data.hasData) {
-              return Container();
-            }
-            return Markdown(
-              padding: EdgeInsets.only(
-                left: 15,
-                right: 15,
-                top: 15,
-                bottom: 15 + getNavBarPadding(context),
-              ),
-              data: data.data!,
-            );
-          },
-        ),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(t.changelogPage.title),
+      ),
+      body: FutureBuilder(
+        future: rootBundle.loadString(Assets.changelog), // ignore: discarded_futures
+        builder: (context, data) {
+          if (!data.hasData) {
+            return Container();
+          }
+          return Markdown(
+            padding: EdgeInsets.only(
+              left: 15,
+              right: 15,
+              top: 15,
+              bottom: 15 + getNavBarPadding(context),
+            ),
+            data: data.data!,
+          );
+        },
       ),
     );
   }
--- lib/widget/watcher/shortcut_watcher.dart
+++ lib/widget/watcher/shortcut_watcher.dart
@@ -3,6 +3,7 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:localsend_app/util/native/platform_check.dart';
+import 'package:routerino/routerino.dart';
 
 class ShortcutWatcher extends StatelessWidget {
   final Widget child;
@@ -19,10 +20,13 @@ class ShortcutWatcher extends StatelessWidget {
         // Add Control+Q binding for Linux
         // https://github.com/localsend/localsend/issues/194
         if (checkPlatform([TargetPlatform.linux])) LogicalKeySet(LogicalKeyboardKey.control, LogicalKeyboardKey.keyQ): _ExitAppIntent(),
+
+        LogicalKeySet(LogicalKeyboardKey.escape): _PopPageIntent(),
       },
       child: Actions(
         actions: {
           _ExitAppIntent: CallbackAction(onInvoke: (_) => exit(0)),
+          _PopPageIntent: CallbackAction(onInvoke: (_) async => Navigator.of(Routerino.context).maybePop()),
         },
         child: child,
       ),
@@ -31,3 +35,5 @@ class ShortcutWatcher extends StatelessWidget {
 }
 
 class _ExitAppIntent extends Intent {}
+
+class _PopPageIntent extends Intent {}
