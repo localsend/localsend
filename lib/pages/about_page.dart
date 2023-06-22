c632533c535786627d3ee9192c69166689920e9b
Tien Do Nam <dev.tien.donam@gmail.com>
2023-06-22 19:05:12 +0200

    feat: bind ESC globally

--- lib/pages/about_page.dart
+++ lib/pages/about_page.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:localsend_app/gen/strings.g.dart';
 import 'package:localsend_app/pages/debug/debug_page.dart';
-import 'package:localsend_app/util/shortcut_warpper.dart';
 import 'package:localsend_app/widget/local_send_logo.dart';
 import 'package:localsend_app/widget/responsive_list_view.dart';
 import 'package:routerino/routerino.dart';
@@ -27,71 +26,66 @@ class AboutPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ShortcutWrapper(
-      child: Scaffold(
-        appBar: AppBar(
-          title: Text(t.aboutPage.title),
-        ),
-        body: ResponsiveListView(
-          padding: const EdgeInsets.symmetric(horizontal: 15),
-          children: [
-            const SizedBox(height: 20),
-            const LocalSendLogo(withText: true),
-            Text(
-              '© ${DateTime.now().year} Tien Do Nam',
-              textAlign: TextAlign.center,
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(t.aboutPage.title),
+      ),
+      body: ResponsiveListView(
+        padding: const EdgeInsets.symmetric(horizontal: 15),
+        children: [
+          const SizedBox(height: 20),
+          const LocalSendLogo(withText: true),
+          Text(
+            '© ${DateTime.now().year} Tien Do Nam',
+            textAlign: TextAlign.center,
+          ),
+          const SizedBox(height: 10),
+          Center(
+            child: TextButton(
+              onPressed: () async {
+                await launchUrl(Uri.parse('https://localsend.org'));
+              },
+              child: const Text('localsend.org'),
             ),
-            const SizedBox(height: 10),
-            Center(
-              child: TextButton(
+          ),
+          Text(_body),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              TextButton(
                 onPressed: () async {
                   await launchUrl(Uri.parse('https://localsend.org'));
                 },
-                child: const Text('localsend.org'),
+                child: const Text('Homepage'),
               ),
-            ),
-            Text(_body),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                TextButton(
-                  onPressed: () async {
-                    await launchUrl(Uri.parse('https://localsend.org'));
-                  },
-                  child: const Text('Homepage'),
-                ),
-                TextButton(
-                  onPressed: () async {
-                    await launchUrl(
-                        Uri.parse('https://github.com/localsend/localsend'),
-                        mode: LaunchMode.externalApplication);
-                  },
-                  child: const Text('Source Code (Github)'),
-                ),
-                TextButton(
-                  onPressed: () async {
-                    await launchUrl(
-                        Uri.parse('https://opensource.org/licenses/MIT'));
-                  },
-                  child: const Text('MIT License'),
-                ),
-                TextButton(
-                  onPressed: () async {
-                    await context.push(() => const LicensePage());
-                  },
-                  child: const Text('License Notices'),
-                ),
-                TextButton(
-                  onPressed: () async {
-                    await context.push(() => const DebugPage());
-                  },
-                  child: const Text('Debugging'),
-                ),
-              ],
-            ),
-            const SizedBox(height: 50),
-          ],
-        ),
+              TextButton(
+                onPressed: () async {
+                  await launchUrl(Uri.parse('https://github.com/localsend/localsend'), mode: LaunchMode.externalApplication);
+                },
+                child: const Text('Source Code (Github)'),
+              ),
+              TextButton(
+                onPressed: () async {
+                  await launchUrl(Uri.parse('https://opensource.org/licenses/MIT'));
+                },
+                child: const Text('MIT License'),
+              ),
+              TextButton(
+                onPressed: () async {
+                  await context.push(() => const LicensePage());
+                },
+                child: const Text('License Notices'),
+              ),
+              TextButton(
+                onPressed: () async {
+                  await context.push(() => const DebugPage());
+                },
+                child: const Text('Debugging'),
+              ),
+            ],
+          ),
+          const SizedBox(height: 50),
+        ],
       ),
     );
   }
