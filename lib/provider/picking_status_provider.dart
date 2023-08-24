5129bc9a9461c80f1aa773a407b4c5fa5c8b81d7
Tien Do Nam <dev.tien.donam@gmail.com>
2023-08-24 21:47:28 +0200

    fix: handle permission error when picking files

--- lib/provider/picking_status_provider.dart
+++
@@ -1,5 +0,0 @@
-import 'package:riverpie_flutter/riverpie_flutter.dart';
-
-/// True, if picking
-/// False, if not picking
-final pickingStatusProvider = StateProvider<bool>((ref) => false);
--- lib/widget/dialogs/loading_dialog.dart
+++ lib/widget/dialogs/loading_dialog.dart
@@ -1,25 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:localsend_app/provider/picking_status_provider.dart';
-import 'package:riverpie_flutter/riverpie_flutter.dart';
-import 'package:routerino/routerino.dart';
 
-class LoadingDialog extends StatefulWidget {
-  const LoadingDialog({Key? key}) : super(key: key);
-
-  @override
-  State<LoadingDialog> createState() => _LoadingDialogState();
-}
-
-class _LoadingDialogState extends State<LoadingDialog> {
-  void _onPickingStatusChanged(bool? prev, bool next) {
-    if (!next) {
-      context.pop();
-    }
-  }
+class LoadingDialog extends StatelessWidget {
+  const LoadingDialog();
 
   @override
   Widget build(BuildContext context) {
-    context.ref.watch(pickingStatusProvider, listener: _onPickingStatusChanged);
     return WillPopScope(
       onWillPop: () => Future.value(false),
       child: const Center(
