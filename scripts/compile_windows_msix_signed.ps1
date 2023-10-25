2ea1c6b6689c8be3049de55be423bfda01abf426
Tien Do Nam <dev.tien.donam@gmail.com>
2023-10-25 21:04:46 +0200

    chore: update msix script

--- scripts/compile_windows_msix_signed.ps1
+++ scripts/compile_windows_msix_signed.ps1
@@ -7,11 +7,14 @@ param(
     [string]$CERTIFICATE_PASSWORD
 )
 
+cd app
 fvm flutter clean
 fvm flutter pub get
 fvm flutter pub run build_runner build -d
-fvm flutter pub run msix:create --certificate-path secrets/windows-tienisto.pfx --certificate-password $CERTIFICATE_PASSWORD
+fvm flutter pub run msix:create --certificate-path ../secrets/windows-tienisto.pfx --certificate-password $CERTIFICATE_PASSWORD
 
 Move-Item -Path build/windows/runner/Release/localsend_app.msix -Destination LocalSend-XXX-windows-x86-64.msix
+
+cd ..
 
 Write-Output 'Generated Signed Windows msix!'
