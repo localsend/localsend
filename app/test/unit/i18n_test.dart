f28f02cf9f29f5e30b467bc05d42477b0719e408
Tien Do Nam <dev.tien.donam@gmail.com>
2023-10-25 22:05:47 +0200

    test: locale codes

--- app/test/unit/i18n_test.dart
+++ app/test/unit/i18n_test.dart
@@ -1,3 +1,4 @@
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:localsend_app/gen/strings.g.dart';
 import 'package:test/test.dart';
 
@@ -7,6 +8,12 @@ void main() {
       // The following test will fail if the i18n file is either not compiled
       // or there are compile-time errors.
       expect(AppLocale.en.translations.general.accept, 'Accept');
+    });
+
+    test('All locales should be supported by Flutter', () async {
+      for (final locale in AppLocale.values) {
+        expect(kMaterialSupportedLanguages, contains(locale.languageCode));
+      }
     });
   });
 }
