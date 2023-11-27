87906d43169b517c056a66f9bb56da4d9d023b23
Tien Do Nam <dev.tien.donam@gmail.com>
2023-11-27 13:36:57 +0100

    docs: update translators.dart

--- app/lib/pages/about/translators.dart
+++ app/lib/pages/about/translators.dart
@@ -4,29 +4,87 @@ part of 'about_page.dart';
 /// The list might be incomplete.
 /// Also write your full name here if you want.
 const _translators = <AppLocale, List<String>>{
-  AppLocale.ar: ['@Adam1141'],
-  AppLocale.bn: ['@RishiASheth'],
-  AppLocale.cs: ['@Amereyeu'],
-  AppLocale.de: ['Tien Do Nam (@Tienisto)'],
-  AppLocale.el: ['Petros Kyladitis (@multipetros)'],
-  AppLocale.en: ['Tien Do Nam (@Tienisto)'],
-  AppLocale.esEs: ['Esteban Daniel Saracho (@esaracho)'],
-  AppLocale.fa: ['@farshad991'],
-  AppLocale.fr: ['@Nixuge'],
-  AppLocale.he: ['@ShlomoCode'],
-  AppLocale.hu: ['@gidano'],
-  AppLocale.it: ['@Francesco146'],
-  AppLocale.ja: ['soya_daizu (@soya-daizu)'],
-  AppLocale.km: ['Chandara H. Wei (@nidexingg)'],
-  AppLocale.ko: ['@mgmix'],
-  AppLocale.pl: ['@Radplay', '@trag1c', '@j-koziel'],
-  AppLocale.ptBr: ['Gabriel Lima (@TheGB0077)', 'Arthur Lobo (@ArthurLobopro)'],
-  AppLocale.th: ['@watchakorn-18k'],
-  AppLocale.tr: ['Mert Şişmanoğlu (@mertssmnoglu)'],
-  AppLocale.ru: ['Sergiy Tkachuk (@sergd88)'],
-  AppLocale.uk: ['Sergiy Tkachuk (@sergd88)'],
-  AppLocale.vi: ['@faea726'],
-  AppLocale.zhCn: ['@nkh0472', '@graphemecluster'],
-  AppLocale.zhHk: ['@graphemecluster'],
-  AppLocale.zhTw: ['@Neo1102', '@graphemecluster'],
+  AppLocale.ar: [
+    '@Adam1141',
+  ],
+  AppLocale.bn: [
+    '@RishiASheth',
+  ],
+  AppLocale.cs: [
+    '@Amereyeu',
+  ],
+  AppLocale.de: [
+    'Tien Do Nam (@Tienisto)',
+  ],
+  AppLocale.el: [
+    'Petros Kyladitis (@multipetros)',
+  ],
+  AppLocale.en: [
+    'Tien Do Nam (@Tienisto)',
+  ],
+  AppLocale.esEs: [
+    'Esteban Daniel Saracho (@esaracho)',
+  ],
+  AppLocale.fa: [
+    '@farshad991',
+  ],
+  AppLocale.filPh: [
+    'Bryan James (@BryanJames16)',
+  ],
+  AppLocale.fr: [
+    '@Nixuge',
+  ],
+  AppLocale.he: [
+    '@ShlomoCode',
+  ],
+  AppLocale.hu: [
+    '@gidano',
+  ],
+  AppLocale.it: [
+    '@Francesco146',
+  ],
+  AppLocale.ja: [
+    'soya_daizu (@soya-daizu)',
+  ],
+  AppLocale.km: [
+    'Chandara H. Wei (@nidexingg)',
+  ],
+  AppLocale.ko: [
+    '@mgmix',
+  ],
+  AppLocale.pl: [
+    '@Radplay',
+    '@trag1c',
+    '@j-koziel',
+  ],
+  AppLocale.ptBr: [
+    'Gabriel Lima (@TheGB0077)',
+    'Arthur Lobo (@ArthurLobopro)',
+  ],
+  AppLocale.ru: [
+    'Sergiy Tkachuk (@sergd88)',
+  ],
+  AppLocale.th: [
+    '@watchakorn-18k',
+  ],
+  AppLocale.tr: [
+    'Mert Şişmanoğlu (@mertssmnoglu)',
+  ],
+  AppLocale.uk: [
+    'Sergiy Tkachuk (@sergd88)',
+  ],
+  AppLocale.vi: [
+    '@faea726',
+  ],
+  AppLocale.zhCn: [
+    '@nkh0472',
+    '@graphemecluster',
+  ],
+  AppLocale.zhHk: [
+    '@graphemecluster',
+  ],
+  AppLocale.zhTw: [
+    '@Neo1102',
+    '@graphemecluster',
+  ],
 };
