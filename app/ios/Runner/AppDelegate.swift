fc274903c3faea293be8ee0b842f4932e444e1d9
Tien Do Nam <38380847+Tienisto@users.noreply.github.com>
2023-12-10 16:20:54 +0100

    feat: remove local network permission check on iOS (#1022)

--- app/ios/Runner/AppDelegate.swift
+++ app/ios/Runner/AppDelegate.swift
@@ -7,27 +7,6 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    let controller : FlutterViewController = window?.rootViewController as! FlutterViewController
-    let channel = FlutterMethodChannel(name:"localsend.localsend_app/iosCall", binaryMessenger: controller.binaryMessenger)
-
-    channel.setMethodCallHandler {
-        (call: FlutterMethodCall, result: @escaping FlutterResult) -> Void in
-        if call.method == "triggerLocalNetworkDialog" {
-            if #available(iOS 14, *) {
-                Task {
-                    let localNetworkAuthorization = LocalNetworkAuthorization()
-                    let granted = await localNetworkAuthorization.requestAuthorization()
-                    result(granted)
-                }
-            } else {
-                // Fallback for older iOS versions
-                result(true)
-            }
-        } else {
-            result(FlutterMethodNotImplemented)
-        }
-    }
-
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
