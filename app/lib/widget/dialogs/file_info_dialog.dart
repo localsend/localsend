809712eaf77546a90c102cb8c0727c64654e5d0c
Caesarovich <cesar@caesarovich.xyz>
2024-06-01 14:06:33 +0200

    Fix file info alert dialog overflowing

--- app/lib/widget/dialogs/file_info_dialog.dart
+++ app/lib/widget/dialogs/file_info_dialog.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:localsend_app/gen/strings.g.dart';
 import 'package:localsend_app/model/persistence/receive_history_entry.dart';
 import 'package:localsend_app/util/file_size_helper.dart';
@@ -13,49 +14,52 @@ class FileInfoDialog extends StatelessWidget {
   Widget build(BuildContext context) {
     return AlertDialog(
       title: Text(t.dialogs.fileInfo.title),
-      content: Table(
-        columnWidths: const {
-          0: IntrinsicColumnWidth(),
-          1: IntrinsicColumnWidth(),
-          2: IntrinsicColumnWidth(),
-        },
-        children: [
-          TableRow(
-            children: [
-              Text(t.dialogs.fileInfo.fileName, maxLines: 1),
-              const SizedBox(width: 10),
-              SelectableText(entry.fileName),
-            ],
-          ),
-          TableRow(
-            children: [
-              Text(t.dialogs.fileInfo.path),
-              const SizedBox(width: 10),
-              SelectableText(entry.savedToGallery ? t.progressPage.savedToGallery : (entry.path ?? '')),
-            ],
-          ),
-          TableRow(
-            children: [
-              Text(t.dialogs.fileInfo.size),
-              const SizedBox(width: 10),
-              SelectableText(entry.fileSize.asReadableFileSize),
-            ],
-          ),
-          TableRow(
-            children: [
-              Text(t.dialogs.fileInfo.sender),
-              const SizedBox(width: 10),
-              SelectableText(entry.senderAlias),
-            ],
-          ),
-          TableRow(
-            children: [
-              Text(t.dialogs.fileInfo.time),
-              const SizedBox(width: 10),
-              SelectableText(entry.timestampString),
-            ],
-          ),
-        ],
+      content: SingleChildScrollView(
+        scrollDirection: Axis.horizontal,
+        child: Table(
+          columnWidths: const {
+            0: IntrinsicColumnWidth(),
+            1: IntrinsicColumnWidth(),
+            2: IntrinsicColumnWidth(),
+          },
+          children: [
+            TableRow(
+              children: [
+                Text(t.dialogs.fileInfo.fileName, maxLines: 1),
+                const SizedBox(width: 10),
+                SelectableText(entry.fileName),
+              ],
+            ),
+            TableRow(
+              children: [
+                Text(t.dialogs.fileInfo.path),
+                const SizedBox(width: 10),
+                SelectableText(entry.savedToGallery ? t.progressPage.savedToGallery : (entry.path ?? '')),
+              ],
+            ),
+            TableRow(
+              children: [
+                Text(t.dialogs.fileInfo.size),
+                const SizedBox(width: 10),
+                SelectableText(entry.fileSize.asReadableFileSize),
+              ],
+            ),
+            TableRow(
+              children: [
+                Text(t.dialogs.fileInfo.sender),
+                const SizedBox(width: 10),
+                SelectableText(entry.senderAlias),
+              ],
+            ),
+            TableRow(
+              children: [
+                Text(t.dialogs.fileInfo.time),
+                const SizedBox(width: 10),
+                SelectableText(entry.timestampString),
+              ],
+            ),
+          ],
+        ),
       ),
       actions: [
         TextButton(
