2d4393a793d3dc37ebc40074ebc99c2edef17e59
Caesarovich <cesar@caesarovich.xyz>
2024-06-01 14:38:00 +0200

    Remove unused import

--- app/lib/widget/dialogs/file_info_dialog.dart
+++ app/lib/widget/dialogs/file_info_dialog.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:localsend_app/gen/strings.g.dart';
 import 'package:localsend_app/model/persistence/receive_history_entry.dart';
 import 'package:localsend_app/util/file_size_helper.dart';
