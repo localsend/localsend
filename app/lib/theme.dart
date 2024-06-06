7c3670e06b3fbb9725f891bfa03906fc8014bfb9
Bharath <106143429+bharathajjarapu@users.noreply.github.com>
2024-06-06 16:10:57 +0530

    added mica to windows

--- app/lib/theme.dart
+++ app/lib/theme.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_acrylic/flutter_acrylic.dart';
 import 'package:localsend_app/gen/strings.g.dart';
 import 'package:localsend_app/model/persistence/color_mode.dart';
 import 'package:localsend_app/provider/device_info_provider.dart';
@@ -7,10 +8,10 @@ import 'package:localsend_app/util/native/platform_check.dart';
 import 'package:localsend_app/util/ui/dynamic_colors.dart';
 import 'package:refena_flutter/refena_flutter.dart';
 import 'package:yaru/yaru.dart' as yaru;
+import 'dart:io';  // Add this import
 
 final _borderRadius = BorderRadius.circular(5);
 
-/// On desktop, we need to add additional padding to achieve the same visual appearance as on mobile
 double get desktopPaddingFix => checkPlatformIsDesktop() ? 8 : 0;
 
 ThemeData getTheme(ColorMode colorMode, Brightness brightness, DynamicColors? dynamicColors) {
@@ -20,57 +21,23 @@ ThemeData getTheme(ColorMode colorMode, Brightness brightness, DynamicColors? dy
 
   final colorScheme = _determineColorScheme(colorMode, brightness, dynamicColors);
 
-  final lightInputBorder = OutlineInputBorder(
+  final inputBorder = OutlineInputBorder(
     borderSide: BorderSide(color: colorScheme.secondaryContainer),
     borderRadius: _borderRadius,
   );
 
-  final darkInputBorder = OutlineInputBorder(
-    borderSide: BorderSide(color: colorScheme.secondaryContainer),
-    borderRadius: _borderRadius,
-  );
-
-  // https://github.com/localsend/localsend/issues/52
-  final String? fontFamily;
-  if (checkPlatform([TargetPlatform.windows])) {
-    fontFamily = switch (LocaleSettings.currentLocale) {
-      AppLocale.ja => 'Yu Gothic UI',
-      AppLocale.ko => 'Malgun Gothic',
-      AppLocale.zhCn => 'Microsoft YaHei UI',
-      AppLocale.zhHk || AppLocale.zhTw => 'Microsoft JhengHei UI',
-      _ => 'Segoe UI Variable Display',
-    };
-  } else {
-    fontFamily = null;
-  }
+  final fontFamily = checkPlatform([TargetPlatform.windows])
+      ? _getWindowsFontFamily()
+      : null;
 
   return ThemeData(
     colorScheme: colorScheme,
     useMaterial3: true,
-    navigationBarTheme: colorScheme.brightness == Brightness.dark
-        ? NavigationBarThemeData(
-            iconTheme: MaterialStateProperty.all(const IconThemeData(color: Colors.white)),
-          )
-        : null,
-    inputDecorationTheme: InputDecorationTheme(
-      filled: true,
-      fillColor: colorScheme.secondaryContainer,
-      border: colorScheme.brightness == Brightness.light ? lightInputBorder : darkInputBorder,
-      focusedBorder: colorScheme.brightness == Brightness.light ? lightInputBorder : darkInputBorder,
-      enabledBorder: colorScheme.brightness == Brightness.light ? lightInputBorder : darkInputBorder,
-      contentPadding: const EdgeInsets.symmetric(vertical: 0, horizontal: 10),
-    ),
-    elevatedButtonTheme: ElevatedButtonThemeData(
-      style: ElevatedButton.styleFrom(
-        foregroundColor: colorScheme.brightness == Brightness.dark ? Colors.white : null,
-        padding: EdgeInsets.symmetric(horizontal: 16, vertical: 8 + desktopPaddingFix),
-      ),
-    ),
-    textButtonTheme: TextButtonThemeData(
-      style: TextButton.styleFrom(
-        padding: EdgeInsets.symmetric(horizontal: 16, vertical: 8 + desktopPaddingFix),
-      ),
-    ),
+    scaffoldBackgroundColor: Colors.transparent,
+    navigationBarTheme: _getNavigationBarTheme(colorScheme),
+    inputDecorationTheme: _getInputDecorationTheme(colorScheme, inputBorder),
+    elevatedButtonTheme: _getElevatedButtonTheme(colorScheme),
+    textButtonTheme: _getTextButtonTheme(),
     fontFamily: fontFamily,
   );
 }
@@ -82,47 +49,27 @@ Future<void> updateSystemOverlayStyle(BuildContext context) async {
 
 Future<void> updateSystemOverlayStyleWithBrightness(Brightness brightness) async {
   if (checkPlatform([TargetPlatform.android])) {
-    // See https://github.com/flutter/flutter/issues/90098
-    final darkMode = brightness == Brightness.dark;
-    final androidSdkInt = RefenaScope.defaultRef.read(deviceInfoProvider).androidSdkInt ?? 0;
-    final bool edgeToEdge = androidSdkInt >= 29;
-
-    SystemChrome.setEnabledSystemUIMode(SystemUiMode.edgeToEdge); // ignore: unawaited_futures
-
-    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
-      statusBarColor: Colors.transparent,
-      statusBarIconBrightness: brightness == Brightness.light ? Brightness.dark : Brightness.light,
-      systemNavigationBarColor: edgeToEdge ? Colors.transparent : (darkMode ? Colors.black : Colors.white),
-      systemNavigationBarContrastEnforced: false,
-      systemNavigationBarIconBrightness: darkMode ? Brightness.light : Brightness.dark,
-    ));
+    await _updateAndroidSystemOverlayStyle(brightness);
   } else {
     SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
-      statusBarBrightness: brightness, // iOS
-      statusBarColor: Colors.transparent, // Not relevant to this issue
+      statusBarBrightness: brightness,
+      statusBarColor: Colors.transparent,
     ));
   }
 }
 
 extension ThemeDataExt on ThemeData {
-  /// This is the actual [cardColor] being used.
   Color get cardColorWithElevation {
     return ElevationOverlay.applySurfaceTint(cardColor, colorScheme.surfaceTint, 1);
   }
 }
 
 extension ColorSchemeExt on ColorScheme {
-  Color get warning {
-    return Colors.orange;
-  }
+  Color get warning => Colors.orange;
 
-  Color? get secondaryContainerIfDark {
-    return brightness == Brightness.dark ? secondaryContainer : null;
-  }
+  Color? get secondaryContainerIfDark => brightness == Brightness.dark ? secondaryContainer : null;
 
-  Color? get onSecondaryContainerIfDark {
-    return brightness == Brightness.dark ? onSecondaryContainer : null;
-  }
+  Color? get onSecondaryContainerIfDark => brightness == Brightness.dark ? onSecondaryContainer : null;
 }
 
 extension InputDecorationThemeExt on InputDecorationTheme {
@@ -135,57 +82,91 @@ ColorScheme _determineColorScheme(ColorMode mode, Brightness brightness, Dynamic
     brightness: brightness,
   );
 
-  final colorScheme = switch (mode) {
+  return switch (mode) {
     ColorMode.system => brightness == Brightness.light ? dynamicColors?.light : dynamicColors?.dark,
     ColorMode.localsend => null,
     ColorMode.oled => (dynamicColors?.dark ?? defaultColorScheme).copyWith(
         background: Colors.black,
         surface: Colors.black,
       ),
-    ColorMode.yaru => throw 'Should reach here',
-  };
-
-  return colorScheme ?? defaultColorScheme;
+    ColorMode.yaru => throw 'Should not reach here',
+  } ?? defaultColorScheme;
 }
 
 ThemeData _getYaruTheme(Brightness brightness) {
   final baseTheme = brightness == Brightness.light ? yaru.yaruLight : yaru.yaruDark;
   final colorScheme = baseTheme.colorScheme;
 
-  final lightInputBorder = OutlineInputBorder(
-    borderSide: BorderSide(color: colorScheme.secondaryContainer),
-    borderRadius: _borderRadius,
-  );
-
-  final darkInputBorder = OutlineInputBorder(
+  final inputBorder = OutlineInputBorder(
     borderSide: BorderSide(color: colorScheme.secondaryContainer),
     borderRadius: _borderRadius,
   );
 
   return baseTheme.copyWith(
-    navigationBarTheme: colorScheme.brightness == Brightness.dark
-        ? NavigationBarThemeData(
-            iconTheme: MaterialStateProperty.all(const IconThemeData(color: Colors.white)),
-          )
-        : null,
-    inputDecorationTheme: InputDecorationTheme(
-      filled: true,
-      fillColor: colorScheme.secondaryContainer,
-      border: colorScheme.brightness == Brightness.light ? lightInputBorder : darkInputBorder,
-      focusedBorder: colorScheme.brightness == Brightness.light ? lightInputBorder : darkInputBorder,
-      enabledBorder: colorScheme.brightness == Brightness.light ? lightInputBorder : darkInputBorder,
-      contentPadding: const EdgeInsets.symmetric(vertical: 0, horizontal: 10),
-    ),
-    elevatedButtonTheme: ElevatedButtonThemeData(
-      style: ElevatedButton.styleFrom(
-        foregroundColor: colorScheme.brightness == Brightness.dark ? Colors.white : null,
-        padding: checkPlatformIsDesktop() ? const EdgeInsets.all(16) : const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-      ),
-    ),
-    textButtonTheme: TextButtonThemeData(
-      style: TextButton.styleFrom(
-        padding: checkPlatformIsDesktop() ? const EdgeInsets.all(16) : const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-      ),
+    navigationBarTheme: _getNavigationBarTheme(colorScheme),
+    inputDecorationTheme: _getInputDecorationTheme(colorScheme, inputBorder),
+    elevatedButtonTheme: _getElevatedButtonTheme(colorScheme),
+    textButtonTheme: _getTextButtonTheme(),
+  );
+}
+
+NavigationBarThemeData? _getNavigationBarTheme(ColorScheme colorScheme) {
+  return colorScheme.brightness == Brightness.dark
+      ? NavigationBarThemeData(
+          iconTheme: MaterialStateProperty.all(const IconThemeData(color: Colors.white)),
+        )
+      : null;
+}
+
+InputDecorationTheme _getInputDecorationTheme(ColorScheme colorScheme, OutlineInputBorder inputBorder) {
+  return InputDecorationTheme(
+    filled: true,
+    fillColor: colorScheme.secondaryContainer,
+    border: inputBorder,
+    focusedBorder: inputBorder,
+    enabledBorder: inputBorder,
+    contentPadding: const EdgeInsets.symmetric(vertical: 0, horizontal: 10),
+  );
+}
+
+ElevatedButtonThemeData _getElevatedButtonTheme(ColorScheme colorScheme) {
+  return ElevatedButtonThemeData(
+    style: ElevatedButton.styleFrom(
+      foregroundColor: colorScheme.brightness == Brightness.dark ? Colors.white : null,
+      padding: EdgeInsets.symmetric(horizontal: 16, vertical: 8 + desktopPaddingFix),
     ),
   );
+}
+
+TextButtonThemeData _getTextButtonTheme() {
+  return TextButtonThemeData(
+    style: TextButton.styleFrom(
+      padding: EdgeInsets.symmetric(horizontal: 16, vertical: 8 + desktopPaddingFix),
+    ),
+  );
+}
+
+Future<void> _updateAndroidSystemOverlayStyle(Brightness brightness) async {
+  final darkMode = brightness == Brightness.dark;
+  final androidSdkInt = RefenaScope.defaultRef.read(deviceInfoProvider).androidSdkInt ?? 0;
+  final bool edgeToEdge = androidSdkInt >= 29;
+
+  await SystemChrome.setEnabledSystemUIMode(SystemUiMode.edgeToEdge);
+  SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+    statusBarColor: Colors.transparent,
+    statusBarIconBrightness: brightness == Brightness.light ? Brightness.dark : Brightness.light,
+    systemNavigationBarColor: edgeToEdge ? Colors.transparent : (darkMode ? Colors.black : Colors.white),
+    systemNavigationBarContrastEnforced: false,
+    systemNavigationBarIconBrightness: darkMode ? Brightness.light : Brightness.dark,
+  ));
+}
+
+String? _getWindowsFontFamily() {
+  return switch (LocaleSettings.currentLocale) {
+    AppLocale.ja => 'Yu Gothic UI',
+    AppLocale.ko => 'Malgun Gothic',
+    AppLocale.zhCn => 'Microsoft YaHei UI',
+    AppLocale.zhHk || AppLocale.zhTw => 'Microsoft JhengHei UI',
+    _ => 'Segoe UI Variable Display',
+  };
 }
