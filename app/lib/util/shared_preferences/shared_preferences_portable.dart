e9595abd194d834af8d5f5493b641c75a8202826
Tien Do Nam <38380847+Tienisto@users.noreply.github.com>
2024-07-25 12:45:58 +0200

    fix: use correct portable settings file when started via autostart (#1562)

--- app/lib/util/shared_preferences/shared_preferences_portable.dart
+++ app/lib/util/shared_preferences/shared_preferences_portable.dart
@@ -1,8 +1,18 @@
+import 'dart:io';
+
 import 'package:localsend_app/util/shared_preferences/shared_preferences_file.dart';
+import 'package:path/path.dart' as path;
 
 /// Custom implementation of SharedPreferencesStorePlatform
 /// that uses a file named settings.json located next to the executable.
 /// This is used to for portable mode.
 class SharedPreferencesPortable extends SharedPreferencesFile {
-  SharedPreferencesPortable() : super(filePath: 'settings.json', beautify: true);
+  SharedPreferencesPortable() : super(filePath: _getSettingsPathFromExecutable(), beautify: true);
+}
+
+/// Returns the absolute path to the settings.json file next to the executable.
+String _getSettingsPathFromExecutable() {
+  final executablePath = Platform.resolvedExecutable;
+  final executableParentPath = File(executablePath).parent.path;
+  return path.join(executableParentPath, 'settings.json');
 }
