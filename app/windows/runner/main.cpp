c11529f05966bf157d102c295e6cef7891e06ac4
FutoTan <35399423+FutoTan@users.noreply.github.com>
2024-08-18 06:41:19 +0800

    [Windows] Use dark title bar on dark system theme. (#1636)

--- app/windows/runner/main.cpp
+++ app/windows/runner/main.cpp
@@ -27,7 +27,7 @@ int APIENTRY wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prev,
   FlutterWindow window(project);
   Win32Window::Point origin(10, 10);
   Win32Window::Size size(500, 600);
-  if (!window.CreateAndShow(L"LocalSend", origin, size)) {
+  if (!window.Create(L"LocalSend", origin, size)) {
     return EXIT_FAILURE;
   }
   window.SetQuitOnClose(true);
