c12199b2b9db67557b6fb2568c3b0107acd9db4b
Davte <davide@davte.it>
2024-08-25 19:18:55 +0200

    Sort directive sections alphabetically and fix indentation

--- app/lib/pages/tabs/receive_tab_vm.dart
+++ app/lib/pages/tabs/receive_tab_vm.dart
@@ -4,8 +4,8 @@ import 'package:localsend_app/model/state/server/server_state.dart';
 import 'package:localsend_app/provider/local_ip_provider.dart';
 import 'package:localsend_app/provider/network/server/server_provider.dart';
 import 'package:localsend_app/provider/settings_provider.dart';
-import 'package:localsend_app/widget/dialogs/quick_save_notice.dart';
 import 'package:localsend_app/widget/dialogs/quick_save_from_favorites_notice.dart';
+import 'package:localsend_app/widget/dialogs/quick_save_notice.dart';
 import 'package:refena_flutter/refena_flutter.dart';
 
 /// Whether the advanced network info is shown
