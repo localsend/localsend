b5ed1e642d0d2eb59919d0784ed1e74dfac7de48
Shlomo <78599753+ShlomoCode@users.noreply.github.com>
2024-09-04 02:57:50 +0300

    feat(macos): add drag-and-drop support for menubar icon (#1716)

--- app/macos/Runner/AppDelegate.swift
+++ app/macos/Runner/AppDelegate.swift
@@ -3,44 +3,122 @@ import FlutterMacOS
 
 @NSApplicationMain
 class AppDelegate: FlutterAppDelegate {
-  override func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
-    // LocalSend handles the close event manually
-    return false
-  }
-
-  // START: method channel logic
-  private var channel: FlutterMethodChannel?
-  private var cachedFiles: [String]? = []
-
-  override func applicationDidFinishLaunching(_ notification: Notification) {
-    let controller = mainFlutterWindow?.contentViewController as! FlutterViewController
-    channel = FlutterMethodChannel(name: "main-delegate-channel", binaryMessenger: controller.engine.binaryMessenger)
-    channel?.setMethodCallHandler(handle)
-  }
-
-  public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-      switch call.method {
-      case "getFiles":
-        result(cachedFiles ?? [])
-        cachedFiles = nil // files has been fetched, no need to cache anymore
-      default:
-        result(FlutterMethodNotImplemented)
-      }
+    override func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        // LocalSend handles the close event manually
+        return false
     }
+    
+    private var statusItem: NSStatusItem?
+    private var channel: FlutterMethodChannel?
+    private var cachedFiles: [String]? = []
+    
+    override func applicationDidFinishLaunching(_ notification: Notification) {
+        let controller = mainFlutterWindow?.contentViewController as! FlutterViewController
+        channel = FlutterMethodChannel(name: "main-delegate-channel", binaryMessenger: controller.engine.binaryMessenger)
+        channel?.setMethodCallHandler(handle)
+        
+        setupStatusBarItem()
+    }
+    
+    private func setupStatusBarItem() {
+        statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.squareLength)
+        if let button = statusItem?.button {
+            let image = NSImage(named: "StatusBarItemIcon")
+            image!.size = NSSize(width: 18, height: 18)
+            image!.isTemplate = true
+            button.image = image
+            
+            let menu = NSMenu()
+            
+            let openItem = NSMenuItem(title: "Open", action: #selector(openApp), keyEquivalent: "o")
+            menu.addItem(openItem)
+            
+            let quitItem = NSMenuItem(title: "Quit LocalSend", action: #selector(quitApp), keyEquivalent: "q")
+            menu.addItem(quitItem)
+            
+            statusItem?.menu = menu
+            
+            let dragView = FileDropView(frame: button.bounds)
+            dragView.appDelegate = self
+            button.addSubview(dragView)
+            
+            dragView.translatesAutoresizingMaskIntoConstraints = false
+            NSLayoutConstraint.activate([
+                dragView.topAnchor.constraint(equalTo: button.topAnchor),
+                dragView.leadingAnchor.constraint(equalTo: button.leadingAnchor),
+                dragView.trailingAnchor.constraint(equalTo: button.trailingAnchor),
+                dragView.bottomAnchor.constraint(equalTo: button.bottomAnchor)
+            ])
+        }
+    }
+    
+    @objc func openApp() {
+        NSApp.activate(ignoringOtherApps: true)
+        mainFlutterWindow?.makeKeyAndOrderFront(nil)
+    }
+    
+    @objc private func quitApp() {
+        NSApp.terminate(nil)
+    }
+    
+    func sendFilesToFlutter(_ filenames: [String]) {
+        cachedFiles?.append(contentsOf: filenames)
+        channel?.invokeMethod("onFiles", arguments: filenames)
+    }
+    
+    // START: handle opened files
+    private func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
+        switch call.method {
+        case "getFiles":
+            result(cachedFiles ?? [])
+            cachedFiles = nil // files has been fetched, no need to cache anymore
+        default:
+            result(FlutterMethodNotImplemented)
+        }
+    }
+    
+    override func application(_ sender: NSApplication, openFile filename: String) -> Bool {
+        sendFilesToFlutter([filename])
+        return true
+    }
+    
+    override func application(_ sender: NSApplication, openFiles filenames: [String]) {
+        sendFilesToFlutter(filenames)
+    }
+    // END: handle opened files
+}
 
-  // END: method channel logic
-
-  // START: handle opened files
-
-  override func application(_ sender: NSApplication, openFile filename: String) -> Bool {
-    cachedFiles?.append(filename)
-    channel?.invokeMethod("onFiles", arguments: [filename])
-    return true
-  }
-
-  override func application(_ sender: NSApplication, openFiles filenames: [String]) {
-    cachedFiles?.append(contentsOf: filenames)
-    channel?.invokeMethod("onFiles", arguments: filenames)
-  }
-  // END: handle opened files
+class FileDropView: NSView {
+    weak var appDelegate: AppDelegate?
+    
+    override init(frame frameRect: NSRect) {
+        super.init(frame: frameRect)
+        setup()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setup()
+    }
+    
+    private func setup() {
+        registerForDraggedTypes([NSPasteboard.PasteboardType.fileURL])
+    }
+    
+    override func draggingEntered(_ sender: NSDraggingInfo) -> NSDragOperation {
+        return .copy
+    }
+    
+    override func performDragOperation(_ sender: NSDraggingInfo) -> Bool {
+        let pasteboard = sender.draggingPasteboard
+        
+        if let fileUrls = pasteboard.readObjects(forClasses: [NSURL.self], options: [.urlReadingFileURLsOnly: true]) as? [URL] {
+            let filenames = fileUrls.map { $0.path }
+            appDelegate?.sendFilesToFlutter(filenames)
+            appDelegate?.openApp()
+            return true
+        }
+        
+        return false
+    }
 }
