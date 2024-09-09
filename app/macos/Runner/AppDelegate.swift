aa838d7e688d69bfa94318d3e8e42487514d9d7b
Shlomo <78599753+ShlomoCode@users.noreply.github.com>
2024-09-09 16:48:27 +0300

    fix(macos): add missing `setActivationPolicy` (#1760)

--- app/macos/Runner/AppDelegate.swift
+++ app/macos/Runner/AppDelegate.swift
@@ -58,7 +58,7 @@ class AppDelegate: FlutterAppDelegate {
             let menu = NSMenu()
             
             let openString = i18n["open"]!
-            let openItem = NSMenuItem(title: openString, action: #selector(openApp), keyEquivalent: "o")
+            let openItem = NSMenuItem(title: openString, action: #selector(showLocalSendFromMenuBar), keyEquivalent: "o")
             menu.addItem(openItem)
             
             let quitString = i18n["quit"]!
@@ -80,9 +80,8 @@ class AppDelegate: FlutterAppDelegate {
         }
     }
     
-    @objc func openApp() {
-        NSApp.activate(ignoringOtherApps: true)
-        mainFlutterWindow?.makeKeyAndOrderFront(nil)
+    @objc func showLocalSendFromMenuBar() {
+        channel?.invokeMethod("showLocalSendFromMenuBar", arguments: nil)
     }
     
     @objc private func quitApp() {
@@ -109,7 +108,8 @@ class AppDelegate: FlutterAppDelegate {
         
         Defaults[.pendingFiles] = []
         Defaults[.pendingStrings] = []
-        openApp()
+
+        channel?.invokeMethod("showLocalSendFromMenuBar", arguments: nil)
     }
     
     // START: handle opened files
@@ -126,13 +126,18 @@ class AppDelegate: FlutterAppDelegate {
             }
             
             result(filePaths)
+
             Defaults[.pendingFiles] = []
-            openApp()
+            
+            channel?.invokeMethod("showLocalSendFromMenuBar", arguments: nil)
         case "getPendingStrings":
             let pendingStrings = Defaults[.pendingStrings]
+
             result(pendingStrings)
+            
             Defaults[.pendingStrings] = []
-            openApp()
+            
+            channel?.invokeMethod("showLocalSendFromMenuBar", arguments: nil)
         case "setupStatusBar":
             let i18n = call.arguments as! [String: String]
             setupStatusBarItem(i18n: i18n)
