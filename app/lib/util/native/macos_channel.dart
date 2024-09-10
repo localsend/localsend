cf48c539fdf36902329f450a8513ace25e9ec7f2
Tien Do Nam <dev.tien.donam@gmail.com>
2024-09-11 00:10:21 +0900

    fix: share extension when LocalSend is not running

--- app/lib/util/native/macos_channel.dart
+++ app/lib/util/native/macos_channel.dart
@@ -18,56 +18,15 @@ Future<void> setupStatusBar() async {
   });
 }
 
-/// Returns a list of files the app was opened with.
-/// This happens:
-/// - on macOS when files are dropped onto the app Dock icon
-/// - on macOS when files are dropped onto the app menu bar icon
-/// - on macOS when files are opened with the app via Finder
-/// - on macOS when files are shared to the app using the share extension (i.e. the system share menu)
-Future<List<String>> getPendingFiles() async {
-  if (defaultTargetPlatform != TargetPlatform.macOS) {
-    return <String>[];
-  }
-
-  final files = await _methodChannel.invokeMethod<List>('getPendingFiles');
-  if (files == null) {
-    throw PlatformException(
-      code: 'UNKNOWN',
-      message: 'Unable to get files',
-    );
-  }
-
-  return files.cast<String>();
-}
-
-/// Returns a list of pending strings from the native swift runner.
-/// This happens:
-/// - on macOS when text is dropped onto the app Dock icon
-/// - on macOS when text is dropped onto the app menu bar icon
-/// - on macOS when text\web link are shared to the app using the share extension (i.e. the system share menu)
-Future<List<String>> getPendingStrings() async {
-  if (defaultTargetPlatform != TargetPlatform.macOS) {
-    return <String>[];
-  }
-
-  final strings = await _methodChannel.invokeMethod<List>('getPendingStrings');
-  if (strings == null) {
-    throw PlatformException(
-      code: 'UNKNOWN',
-      message: 'Unable to get strings',
-    );
-  }
-
-  return strings.cast<String>();
-}
-
 final _pendingFilesStreamController = StreamController<List<String>>.broadcast();
 Stream<List<String>> get pendingFilesStream => _pendingFilesStreamController.stream;
 
 final _pendingStringsStreamController = StreamController<List<String>>.broadcast();
 Stream<List<String>> get pendingStringsStream => _pendingStringsStreamController.stream;
 
-void setupMethodCallHandler() {
+/// Sets up the method call handler.
+/// Any call from native code is dropped until this method is called.
+Future<void> setupMethodCallHandler() async {
   _methodChannel.setMethodCallHandler((call) async {
     switch (call.method) {
       case 'onPendingFiles':
@@ -81,4 +40,6 @@ void setupMethodCallHandler() {
         break;
     }
   });
+
+  await _methodChannel.invokeMethod('methodChannelInitialized');
 }
