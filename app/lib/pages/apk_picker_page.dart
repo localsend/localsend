e79f81c71e6d49ec139a9c484c6019618b2d8e81
Kishore Kumar S <livekishore2001@gmail.com>
2024-10-08 19:01:24 +0530

    Select Multiple APK at once

--- app/lib/pages/apk_picker_page.dart
+++ app/lib/pages/apk_picker_page.dart
@@ -22,11 +22,23 @@ class ApkPickerPage extends StatefulWidget {
 
 class _ApkPickerPageState extends State<ApkPickerPage> with Refena {
   final _textController = TextEditingController();
+  final List<Application> _selectedApps = [];
 
-  void _pickApp(Application app) {
+  Future<void> _pickApps(List<Application> apps) async {
     // ignore: discarded_futures
-    ref.redux(selectedSendingFilesProvider).dispatchAsync(AddFilesAction(files: [app], converter: CrossFileConverters.convertApplication));
-    context.pop();
+
+    for (Application app in apps) {
+      await ref
+          .redux(selectedSendingFilesProvider)
+          .dispatchAsync(AddFilesAction(
+            files: [app],
+            converter: CrossFileConverters.convertApplication,
+          ));
+    }
+
+    if (mounted) {
+      context.pop();
+    }
   }
 
   @override
@@ -34,6 +46,16 @@ class _ApkPickerPageState extends State<ApkPickerPage> with Refena {
     _textController.dispose();
     ref.dispose(apkSearchParamProvider);
     super.dispose();
+  }
+
+  void _appSelection(Application app) {
+    setState(() {
+      if (_selectedApps.contains(app)) {
+        _selectedApps.remove(app);
+      } else {
+        _selectedApps.add(app);
+      }
+    });
   }
 
   @override
@@ -61,15 +83,32 @@ class _ApkPickerPageState extends State<ApkPickerPage> with Refena {
           }, onSelected: (value) {
             switch (value) {
               case 0:
-                ref.notifier(apkSearchParamProvider).setState((old) => old.copyWith(includeSystemApps: !old.includeSystemApps));
+                ref.notifier(apkSearchParamProvider).setState((old) =>
+                    old.copyWith(includeSystemApps: !old.includeSystemApps));
                 break;
               case 1:
-                ref.notifier(apkSearchParamProvider).setState((old) => old.copyWith(onlyAppsWithLaunchIntent: !old.onlyAppsWithLaunchIntent));
+                ref.notifier(apkSearchParamProvider).setState((old) =>
+                    old.copyWith(
+                        onlyAppsWithLaunchIntent:
+                            !old.onlyAppsWithLaunchIntent));
                 break;
             }
           }),
         ],
       ),
+      floatingActionButton: (_selectedApps.isEmpty)
+          ? Container()
+          : FloatingActionButton.extended(
+              onPressed: () async => await _pickApps(_selectedApps),
+              label: Row(
+                children: [
+                  const Icon(Icons.add),
+                  const SizedBox(width: 5),
+                  Text(
+                      'Add ${_selectedApps.length} ${(_selectedApps.length == 1) ? "App" : "Apps"}'),
+                ],
+              ),
+            ),
       body: ResponsiveListView.single(
         padding: const EdgeInsets.symmetric(horizontal: 15),
         tabletPadding: const EdgeInsets.symmetric(horizontal: 15),
@@ -86,7 +125,9 @@ class _ApkPickerPageState extends State<ApkPickerPage> with Refena {
                   controller: _textController,
                   autofocus: true,
                   onChanged: (s) {
-                    ref.notifier(apkSearchParamProvider).setState((old) => old.copyWith(query: s));
+                    ref
+                        .notifier(apkSearchParamProvider)
+                        .setState((old) => old.copyWith(query: s));
                     setState(() {});
                   },
                   decoration: InputDecoration(
@@ -99,7 +140,9 @@ class _ApkPickerPageState extends State<ApkPickerPage> with Refena {
                     suffixIcon: apkParams.query.isNotEmpty
                         ? IconButton(
                             onPressed: () {
-                              ref.notifier(apkSearchParamProvider).setState((old) => old.copyWith(query: ''));
+                              ref
+                                  .notifier(apkSearchParamProvider)
+                                  .setState((old) => old.copyWith(query: ''));
                               _textController.clear();
                             },
                             icon: const Icon(Icons.clear),
@@ -127,7 +170,7 @@ class _ApkPickerPageState extends State<ApkPickerPage> with Refena {
                       return Padding(
                         padding: const EdgeInsets.only(bottom: 10),
                         child: InkWell(
-                          onTap: () => _pickApp(app),
+                          onTap: () => _appSelection(app),
                           customBorder: RoundedRectangleBorder(
                             borderRadius: BorderRadius.circular(10),
                           ),
@@ -151,21 +194,28 @@ class _ApkPickerPageState extends State<ApkPickerPage> with Refena {
                                     ),
                                     Consumer(
                                       builder: (context, ref) {
-                                        final appSize = ref.watch(apkSizeProvider(app.apkFilePath));
+                                        final appSize = ref.watch(
+                                            apkSizeProvider(app.apkFilePath));
                                         final appSizeString = appSize.maybeWhen(
-                                          data: (size) => '${size.asReadableFileSize} • ',
+                                          data: (size) =>
+                                              '${size.asReadableFileSize} • ',
                                           orElse: () => '',
                                         );
                                         return Column(
-                                          crossAxisAlignment: CrossAxisAlignment.start,
+                                          crossAxisAlignment:
+                                              CrossAxisAlignment.start,
                                           children: [
                                             Text(
                                               '$appSizeString${app.versionName != null ? 'v${app.versionName}' : ''}',
-                                              style: Theme.of(context).textTheme.bodySmall,
+                                              style: Theme.of(context)
+                                                  .textTheme
+                                                  .bodySmall,
                                             ),
                                             Text(
                                               app.packageName,
-                                              style: Theme.of(context).textTheme.bodySmall,
+                                              style: Theme.of(context)
+                                                  .textTheme
+                                                  .bodySmall,
                                             ),
                                           ],
                                         );
@@ -174,6 +224,14 @@ class _ApkPickerPageState extends State<ApkPickerPage> with Refena {
                                   ],
                                 ),
                               ),
+                              Icon(
+                                _selectedApps.contains(app)
+                                    ? Icons.check_circle
+                                    : Icons.radio_button_unchecked,
+                                color: _selectedApps.contains(app)
+                                    ? Theme.of(context).iconTheme.color
+                                    : Colors.grey,
+                              )
                             ],
                           ),
                         ),
@@ -194,7 +252,7 @@ class _ApkPickerPageState extends State<ApkPickerPage> with Refena {
               },
             ),
             SliverToBoxAdapter(
-              child: SizedBox(height: getNavBarPadding(context) + 30), // handle navigation bar + some additional padding
+              child: SizedBox(height: getNavBarPadding(context) + 50),
             ),
           ],
         ),
