f56d6bde86ffb78dabc2ef0cc434eea0d8f76ba1
Tien Do Nam <38380847+Tienisto@users.noreply.github.com>
2024-11-03 00:33:22 +0100

    feat: use rhttp (#1988)

--- common/lib/isolate.dart
+++ common/lib/isolate.dart
@@ -1,3 +1,4 @@
+export 'package:common/src/isolate/child/http_provider.dart' show CustomHttpClient, CustomCancelToken;
 export 'package:common/src/isolate/child/sync_provider.dart';
 export 'package:common/src/isolate/child/upload_isolate.dart' show UriContentStreamResolver;
 export 'package:common/src/isolate/parent/actions.dart';
