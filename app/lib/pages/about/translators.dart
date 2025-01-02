572955f1e047c7c0466a8878db4b27cedfb8075e
Tien Do Nam <dev.tien.donam@gmail.com>
2025-01-03 00:03:27 +0100

    chore: format

--- app/lib/pages/about/translators.dart
+++ app/lib/pages/about/translators.dart
@@ -80,7 +80,7 @@ const _translators = <AppLocale, List<String>>{
     'Sergiy Tkachuk (@sergd88)',
   ],
   AppLocale.sr: [
-    'Nebojsa Tomic (@nebojsatomic)', 
+    'Nebojsa Tomic (@nebojsatomic)',
     'Nemanja Dimitrijevic (@nemanya-d)',
   ],
   AppLocale.th: [
