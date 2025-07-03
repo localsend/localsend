38122998810c825055eda962b71469d7351e9e0a
Chenx Dust <chenx_dust@outlook.com>
2025-07-04 01:20:15 +0800

    feat: support windows share sheet

---
+++ scripts/compile_windows_msix_helper.ps1
@@ -0,0 +1,4 @@
+# Using Visual Studio 2022 Developer PowerShell
+# or using e.g. "C:\Program Files (x86)\Windows Kits\10\bin\10.0.26100.0\x64\makeappx.exe"
+
+MakeAppx.exe pack /o /d .\msix /nv /p .\app\windows\localsend_msix_helper.msix
