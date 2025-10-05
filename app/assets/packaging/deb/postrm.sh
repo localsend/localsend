118de97b460719669163a7e7d508b8be9ead0ee7
Gabbar <gabbar.silver@gmail.com>
2025-10-05 20:33:09 +0530

    refator

--- app/assets/packaging/deb/postrm.sh
+++ app/assets/packaging/deb/postrm.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 set -e
 
+echo "Sorry to see you go :("
+
 # Remove UFW rules if they exist
 if command -v ufw >/dev/null 2>&1; then
     ufw delete allow LocalSend 2>/dev/null || true
 fi
 
-echo "Sorry to see you go :("
-
+UFW_FILENAME="ufw-localsend"
 UFW_PROFILE_DIR="/usr/share/localsend_app/data/flutter_assets/assets/packaging"
 UFW_SYSTEM_DIR="/etc/ufw/applications.d"
 
-# Remove UFW profile on purge
-if [ "$1" = "purge" ]; then
+# Remove UFW profile
+if [ -f "$UFW_PROFILE_DIR/ufw-localsend" ]; then
     rm -f "$UFW_PROFILE_DIR/ufw-localsend"
 fi
 
