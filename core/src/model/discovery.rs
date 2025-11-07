e0749143fcd1feb30f6296106af50eb3d83a3058
the Lengue E <11303106+the-lengue-e@user.noreply.gitee.com>
2025-11-07 14:10:06 +0800

    Fix Windows build and Rust DTO definitions

--- core/src/model/discovery.rs
+++ core/src/model/discovery.rs
@@ -9,3 +9,35 @@ pub enum DeviceType {
     Headless,
     Server,
 }
+
+#[derive(Clone, Debug, Deserialize, Eq, Serialize, PartialEq)]
+#[serde(rename_all = "lowercase")]
+pub enum ProtocolType {
+    Http,
+    Https,
+}
+
+#[derive(Clone, Debug, Deserialize, Serialize, PartialEq)]
+#[serde(rename_all = "camelCase")]
+pub struct RegisterDto {
+    pub alias: String,
+    pub version: String,
+    pub device_model: Option<String>,
+    pub device_type: Option<DeviceType>,
+    pub fingerprint: String,
+    pub port: u16,
+    pub protocol: ProtocolType,
+    pub download: bool,
+}
+
+#[derive(Clone, Debug, Deserialize, Serialize, PartialEq)]
+#[serde(rename_all = "camelCase")]
+pub struct RegisterResponseDto {
+    pub alias: String,
+    pub version: String,
+    pub device_model: Option<String>,
+    pub device_type: Option<DeviceType>,
+    pub fingerprint: String,
+    pub download: bool,
+}
+
