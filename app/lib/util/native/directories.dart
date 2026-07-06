ef42b5c579f09b753778b8b1bd9a1c65aa6dcece
Vojta Netrh <73762553+Nert53@users.noreply.github.com>
2026-07-06 21:15:01 +0200

    function to get save directory by file type

--- app/lib/util/native/directories.dart
+++ app/lib/util/native/directories.dart
@@ -1,6 +1,8 @@
 import 'dart:io' show Directory, Platform;
 
 import 'package:flutter/foundation.dart';
+import 'package:localsend_app/model/state/settings_state.dart';
+import 'package:mime/mime.dart';
 import 'package:path_provider/path_provider.dart' as path;
 
 Future<String> getDefaultDestinationDirectory() async {
@@ -34,4 +36,31 @@ Future<String> getDefaultDestinationDirectory() async {
 
 Future<String> getCacheDirectory() async {
   return (await path.getTemporaryDirectory()).path;
+}
+
+Future<String> getDestionationDirectoryByFileType(String fileName, SettingsState settings) async {
+  final defaultDestination = await getDefaultDestinationDirectory();
+  final String? rawMime = lookupMimeType(fileName);
+  print('fileName: $fileName, rawMime: $rawMime');
+  if (rawMime == null) {
+    return settings.destination ?? defaultDestination;
+  }
+
+  // some mime types are "application" but users see it as documents
+  // "application/vnd.openxmlformats-officedocument.wordprocessingml.document" is docx
+  if (rawMime.contains('document') || rawMime.contains('pdf')) {
+    return settings.documentsDestination ?? defaultDestination;
+  }
+
+  // other types has "correct" type in first part
+  switch (rawMime.split('/').first) {
+    case 'text':
+      return settings.documentsDestination ?? defaultDestination;
+    case 'image' || 'video':
+      return settings.mediaDestination ?? defaultDestination;
+    case 'audio':
+      return settings.destination ?? defaultDestination;
+    default:
+      return settings.destination ?? defaultDestination;
+  }
 }
