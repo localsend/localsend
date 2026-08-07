ae0d2c868c5635b726622256c18c6157e18f9056
antheducation <antheducation@gmail.com>
2026-08-07 15:27:30 +0300

    fix(app): repair the single-instance show handover (#3246)

--- app/lib/config/init.dart
+++ app/lib/config/init.dart
@@ -1,5 +1,4 @@
 import 'dart:async';
-import 'dart:convert';
 import 'dart:io';
 
 import 'package:dart_mappable/dart_mappable.dart';
@@ -39,14 +38,13 @@ import 'package:localsend_app/util/native/tray_helper.dart';
 import 'package:localsend_app/util/notification_strings.dart';
 import 'package:localsend_app/util/ui/dynamic_colors.dart';
 import 'package:localsend_app/util/ui/snackbar.dart';
-import 'package:localsend_isolates/api_route_builder.dart';
-import 'package:localsend_isolates/constants.dart';
 import 'package:localsend_isolates/isolate.dart';
 import 'package:localsend_isolates/model/dto/file_dto.dart';
 import 'package:localsend_isolates/model/dto/multicast_dto.dart';
 import 'package:localsend_isolates/rust/api/logging.dart' as rust_logging;
 import 'package:localsend_isolates/rust/frb_generated.dart';
 import 'package:localsend_isolates/util/logger.dart';
+import 'package:localsend_isolates/util/show_instance.dart';
 import 'package:localsend_isolates/util/transfer_notification.dart';
 import 'package:logging/logging.dart';
 import 'package:refena_flutter/addons.dart';
@@ -93,35 +91,15 @@ Future<RefenaContainer> preInit(List<String> args) async {
     // Check if this app is already open and let it "show up".
     // If this is the case, then exit the current instance.
 
-    final client = HttpClient();
-    client.connectionTimeout = const Duration(milliseconds: 100);
-    client.badCertificateCallback = (cert, host, port) => true;
-
-    try {
-      final uri =
-          Uri.parse(
-            ApiRoute.show.targetRaw(
-              '127.0.0.1',
-              persistenceService.getPort(),
-              persistenceService.isHttps(),
-              peerProtocolVersion,
-            ),
-          ).replace(
-            queryParameters: {
-              'token': persistenceService.getShowToken(),
-            },
-          );
-
-      final request = await client.postUrl(uri);
-      request.headers.contentType = ContentType.json;
-      request.write(jsonEncode({'args': args}));
-      final response = await request.close().timeout(const Duration(milliseconds: 500));
-      if (response.statusCode == 200) {
-        exit(0); // Another instance does exist
-      }
-    } catch (_) {
-    } finally {
-      client.close(force: true);
+    final handedOver = await notifyRunningInstance(
+      securityContext: persistenceService.getSecurityContext(),
+      port: persistenceService.getPort(),
+      https: persistenceService.isHttps(),
+      showToken: persistenceService.getShowToken(),
+      args: args,
+    );
+    if (handedOver) {
+      exit(0); // Another instance does exist
     }
 
     // initialize tray AFTER i18n has been initialized
